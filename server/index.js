1df20e32fe08418528e635598fe1098a87dbe727
Zireael03 <egorstukalkin@yandex.ru>
2024-04-09 17:31:58 +0400

    Сделан базовый рабочий роутинг. Подготовка к методам бд. P.s. детали в предыдущем коммите.

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
-//Обработка ошибок всегда в конце
+//Обработка ошибок всегда в конце, последний Middleware
 app.use(errorHandler)
 
 app.get('/',(req, res)=>{
