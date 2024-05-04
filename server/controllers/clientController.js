7af0af8f0c919bd3632243bfe5e405470510672a
maritolli <mashamitr6556@gmail.com>
2024-05-04 12:48:44 +0400

    Исправленные запросы

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -1,13 +1,25 @@
 const {clients, orders} = require('../models/models')
 const ApiError = require('../error/ApiError');
 
+const bcrypt = require ('bcrypt')
+
 class ClientController{
-    async registration(req, res){
-        const {Fname, Login, Password} = req.body;
-        const Client = await clients.create({Fname, Login, Password});
+    async registration(req, res, next){
+        const {Fname, Login, Password} = req.body; //получили из тела запроса
+        if (!Fname || !Login || !Password){ //если что-то не ввели
+            return next(ApiError.badRequest('Некорректный email, пароль или имя пользователя'))
+        }
+        const candidate = await clients.findOne({where: {Login}}) //проверка на существование пользователя
+        if (candidate){ //если вернулся и непустой
+            return next(ApiError.badRequest('Пользователь с таким логином уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5) //хэшируем пароль
+        const Client = await clients.create({Fname, Login, Password: hashPassword});
+        const orders
         return res.json(Client);
 
     }
+
     async login(req, res){
 
     }
